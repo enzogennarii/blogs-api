b6d640849113a1916009402ff547d1089bd061a5
Enzo Gennari <enzo.gennari02@gmail.com>
2023-08-26 11:23:02 -0300

    Requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false,
+  });
+
+  return User;
+};
