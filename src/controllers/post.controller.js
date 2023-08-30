116ca03d037c6d50b3a5b8c382a81040e4756faa
Enzo Gennari <enzo.gennari02@gmail.com>
2023-08-30 19:52:34 -0300

    Diminue o número de linhas dos arquivos post.controller e post.service

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -3,33 +3,25 @@ const mapStatusHTTP = require('../utils/mapStatusHTTP');
 
 const post = async (req, res) => {
   try {
-    const { userId } = req;
     const { title, content, categoryIds } = req.body;
-    const postData = { title, content, categoryIds, userId };
+    const postData = { title, content, categoryIds, userId: req.userId };
     const { status, data } = await postService.post(postData);
     res.status(mapStatusHTTP(status)).json(data);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+  } catch (e) { res.status(500).json({ message: e.message }); }
 };
 
 const getAll = async (_req, res) => {
   try {
     const { status, data } = await postService.getAll();
     res.status(mapStatusHTTP(status)).json(data);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+  } catch (e) { res.status(500).json({ message: e.message }); }
 };
 
 const getById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { status, data } = await postService.getById(id);
+    const { status, data } = await postService.getById(req.params.id);
     res.status(mapStatusHTTP(status)).json(data);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+  } catch (e) { res.status(500).json({ message: e.message }); }
 };
 
 const update = async (req, res) => {
@@ -38,42 +30,25 @@ const update = async (req, res) => {
     const { title, content } = req.body;
     const { status, data } = await postService.update({ id, title, content });
     res.status(mapStatusHTTP(status)).json(data);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+  } catch (e) { res.status(500).json({ message: e.message }); }
 };
 
 const remove = async (req, res) => {
   try {
-    const { id } = req.params;
-    const serviceResponse = await postService.remove(id);
-
+    const serviceResponse = await postService.remove(req.params.id);
     if (serviceResponse.status !== 'NO_CONTENT') {
       const { status, data } = serviceResponse;
       res.status(mapStatusHTTP(status)).json(data);
     }
-
     res.status(mapStatusHTTP(serviceResponse.status)).end();
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+  } catch (e) { res.status(500).json({ message: e.message }); }
 };
 
 const getByQuery = async (req, res) => {
   try {
-    const { q } = req.query;
-    const { status, data } = await postService.getByQuery(q);
+    const { status, data } = await postService.getByQuery(req.query.q);
     res.status(mapStatusHTTP(status)).json(data);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+  } catch (e) { res.status(500).json({ message: e.message }); }
 };
 
-module.exports = {
-  post,
-  getAll,
-  getById,
-  update,
-  remove,
-  getByQuery,
-};
+module.exports = { post, getAll, getById, update, remove, getByQuery };
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,96 +1,58 @@
 const { Op } = require('sequelize');
 const { BlogPost, PostCategory, User, Category, sequelize } = require('../models');
 
+const postNotFoundResponseObj = { status: 'NOT_FOUND', data: { message: 'Post does not exist' } };
+const includeKeyStructure = [{ model: User, as: 'user', attributes: { exclude: ['password'] } },
+  { model: Category, as: 'categories', through: { attributes: [] } }];
+
 const post = async ({ userId, title, content, categoryIds }) => {
   const transaction = await sequelize.transaction();
-
   const newPost = await BlogPost.create({ title, content, userId }, { transaction });
-
   const promises = categoryIds.map((id) => PostCategory.create({
     postId: newPost.id, categoryId: id,
   }, { transaction }));
   await Promise.all(promises);
-
   await transaction.commit();
-
   return { status: 'CREATED', data: newPost };
 };
 
 const getAll = async () => {
-  const posts = await BlogPost.findAll({
-    include: [
-      { model: User, as: 'user', attributes: { exclude: ['password'] } },
-      { model: Category, as: 'categories', through: { attributes: [] } },
-    ],
-  });
+  const posts = await BlogPost.findAll({ include: includeKeyStructure });
   return { status: 'SUCCESSFUL', data: posts };
 };
 
 const getById = async (id) => {
-  const postId = Number(id);
-  const blogPost = await BlogPost.findOne({
-    where: { id: postId },
-    include: [
-      { model: User, as: 'user', attributes: { exclude: ['password'] } },
-      { model: Category, as: 'categories', through: { attributes: [] } },
-    ],
-  });
-
-  if (!blogPost) {
-    return { status: 'NOT_FOUND', data: { message: 'Post does not exist' } };
-  }
-
+  const blogPost = await BlogPost.findOne({ where: { id: Number(id) },
+  include: includeKeyStructure });
+  if (!blogPost) return postNotFoundResponseObj;
   return { status: 'SUCCESSFUL', data: blogPost };
 };
 
 const update = async ({ id, title, content }) => {
-  const postId = Number(id);
-  await BlogPost.update({ title, content }, { where: { id: postId } });
-  const updatedPost = (await getById(postId)).data;
+  await BlogPost.update({ title, content }, { where: { id: Number(id) } });
+  const updatedPost = (await getById(id)).data;
   return { status: 'SUCCESSFUL', data: updatedPost };
 };
 
 const remove = async (postId) => {
-  const id = Number(postId);
   const transaction = await sequelize.transaction();
-
   const results = await Promise.all([
-    BlogPost.destroy({ where: { id }, transaction }),
+    BlogPost.destroy({ where: { id: Number(postId) }, transaction }),
   ]);
-
-  const postNotFound = results.some((deletedRows) => !deletedRows);
-
-  if (postNotFound) {
-    return { status: 'NOT_FOUND', data: { message: 'Post does not exist' } };
-  }
-
+  if (results.some((deletedRows) => !deletedRows)) return postNotFoundResponseObj;
   await transaction.commit();
-
   return { status: 'NO_CONTENT' };
 };
 
 const getByQuery = async (query) => {
   const postsResearched = await BlogPost.findAll({
-    where: {
-      [Op.or]: [
-        { title: { [Op.like]: `%${query || ''}%` } },
-        { content: { [Op.like]: `%${query || ''}%` } },
-      ],
-    },
-    include: [
-      { model: User, as: 'user', attributes: { exclude: ['password'] } },
-      { model: Category, as: 'categories', through: { attributes: [] } },
-    ],
+    where: { [Op.or]: [
+      { title: { [Op.like]: `%${query || ''}%` } },
+      { content: { [Op.like]: `%${query || ''}%` } },
+    ] },
+    include: includeKeyStructure,
   });
-
   return { status: 'SUCCESSFUL', data: postsResearched };
 };
 
-module.exports = {
-  post,
-  getAll,
-  getById,
-  update,
-  remove,
-  getByQuery,
-};
+module.exports = { post, getAll, getById, update, remove, getByQuery };
